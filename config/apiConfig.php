933cb5b588d0eeb73321b2dde907d2986469c841
ety001 <ety001@domyself.me>
2013-06-08 01:53:38 +0800

    edit the api config file's content

--- config/apiConfig.php
+++ config/apiConfig.php
@@ -1,8 +1,11 @@
 <?php
 return array(
 	'renren' => array(
-		'api' => '',
-		'secret' => ''
+		'APIKEY' => '39ca00577c3b47aa9140c89319938b15',
+		'SecretKey' => '694235c392734c168fdbdf16ae09df1e',
+		'APIURL' => 'http://api.renren.com/restserver.do',
+		'APIVersion' => '1.0',
+		'decodeFormat' => 'json'
 		),
 	'qqzone' => array(
 		'api' => '',

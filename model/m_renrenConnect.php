0707e8efb07b8cb2c30e586c95315d5e4a5ffe0b
ety001 <ety001@domyself.me>
2013-06-15 18:48:34 +0800

    database table model

---
+++ model/m_renrenConnect.php
@@ -0,0 +1,6 @@
+<?php
+class m_renrenConnect extends spModel
+{
+  var $pk = "id"; // 数据表的主键
+  var $table = "renren_connect"; // 数据表的名称
+}
---
+++ model/m_user.php
@@ -0,0 +1,6 @@
+<?php
+class m_user extends spModel
+{
+  var $pk = "id"; // 数据表的主键
+  var $table = "user"; // 数据表的名称
+}

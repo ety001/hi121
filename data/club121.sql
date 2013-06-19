26904400ea281202924feec6463d8e8bd8e639b4
ety001 <ety001@domyself.me>
2013-06-19 23:43:00 +0800

    change the database struct

--- data/club121.sql
+++ data/club121.sql
@@ -3,18 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: 127.0.0.1
--- 生成日期: 2013 年 06 月 15 日 22:40
+-- 生成日期: 2013 年 06 月 19 日 23:41
 -- 服务器版本: 5.6.10
 -- PHP 版本: 5.3.15
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- 数据库: `club121`
@@ -33,7 +27,25 @@ CREATE TABLE IF NOT EXISTS `club121_renren_connect` (
   `refresh_token` text NOT NULL,
   `expires_time` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `club121_task`
+--
+
+CREATE TABLE IF NOT EXISTS `club121_task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL COMMENT 'user_id',
+  `condition` varchar(100) NOT NULL COMMENT '条件模块',
+  `last_condition` text NOT NULL COMMENT '上次执行后的条件内容',
+  `command` varchar(100) NOT NULL COMMENT '执行模块',
+  `status` int(2) NOT NULL DEFAULT '1' COMMENT '任务状态，0不可用，1可用',
+  `add_time` int(11) NOT NULL COMMENT '添加任务时间戳',
+  `last_time` int(11) NOT NULL COMMENT '上次执行执行',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -46,8 +58,4 @@ CREATE TABLE IF NOT EXISTS `club121_user` (
   `nickname` text NOT NULL COMMENT '昵称',
   `renren_id` int(18) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;

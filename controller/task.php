31f57603a6fbf32d67f2fa983958056c6d738ef7
ety001 <ety001@domyself.me>
2013-06-19 21:59:07 +0800

    成功获取绑定帐号的人人状态

--- controller/task.php
+++ controller/task.php
@@ -2,13 +2,20 @@
 class task extends spController
 {
 	function index(){
+		global $apiConfig;
 		$renrenConnectModel = spClass('m_renrenConnect');
 		$arr = $renrenConnectModel->find();
 		var_dump($arr['access_token']);
 
 		import('include/renren/RenrenRestApiService.class.php');
 		$rrConct = spClass('RenrenRestApiService');
-		$rrConct->rr_post_curl();
-		var_dump($rrConct);
+		$apiCommitInfo = array(
+			'v' => $apiConfig['renren']['APIVersion'],
+			'access_token' => $arr['access_token'],
+			'page' => 1,
+			'count' => 1
+		);
+		$result = $rrConct->rr_post_curl('status.gets',$apiCommitInfo);
+		var_dump($result);
 	}
 }

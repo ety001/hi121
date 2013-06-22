d0dc477f29346c8c815dfdcfa99665e97483db85
ety001 <ety001@domyself.me>
2013-06-22 23:50:56 +0800

    人人网相关功能接口

---
+++ include/renren/renren.class.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * 人人网相关功能封装
+ *
+ * @Created: 17:09:40 2013/6/22
+ * @Author:	ETY001 <ety002@gmail.com>
+ * @Blog:	http://www.domyself.me
+ */
+
+require_once 'RenrenRestApiService.class.php';
+class renren{
+	private 
+}
